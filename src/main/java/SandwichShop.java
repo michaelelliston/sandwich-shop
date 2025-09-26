02e0cbcc0db10d4eb0d41dfda4e30192341f1dfd
Michael <michael.elliston95@gmail.com>
2025-09-26 14:18:43 -0500

    fourth commit

--- src/main/java/SandwichShop.java
+++ src/main/java/SandwichShop.java
@@ -10,6 +10,9 @@ public class SandwichShop {
         double sandwichPrice = 0;
         double studentDiscount = 0.1;
         double seniorDiscount = 0.2;
+        double loadedRegular = 1.00;
+        double loadedLarge = 1.75;
+        double loadedInput;
 
         System.out.println("Welcome! Please select a size: \n1. Regular: Base price $5.45\n2. Large: Base price $8.95");
         String input = myScanner.nextLine();
@@ -17,26 +20,29 @@ public class SandwichShop {
 
         if (sandwichSize == 1) {
             sandwichPrice = regularSandwichPrice;
-        }
-        else if (sandwichSize == 2) {
+        } else if (sandwichSize == 2) {
             sandwichPrice = largeSandwichPrice;
-        }
-        else System.err.println("Invalid selection.");
+        } else System.err.println("Invalid selection.");
 
+        System.out.println("Would you like your sandwich to be loaded?\n1. Yes\n2. No");
+        input = myScanner.nextLine();
+        loadedInput = Double.parseDouble(input);
+        if (loadedInput == 1 && sandwichSize == 1) {
+            sandwichPrice = sandwichPrice + loadedRegular;
+        } else if (loadedInput == 1 && sandwichSize == 2) {
+            sandwichPrice = sandwichPrice + loadedLarge;
+        }
         System.out.println("Next, please input your age:");
         input = myScanner.nextLine();
         double customerAge = Float.parseFloat(input);
 
         if (customerAge < 18 && customerAge > 0) {
             sandwichPrice = sandwichPrice - (sandwichPrice * studentDiscount);
-        }
-        else if (customerAge >= 65) {
+        } else if (customerAge >= 65) {
             sandwichPrice = sandwichPrice - (sandwichPrice * seniorDiscount);
-        }
-        else if (customerAge < 65 && customerAge >= 18) { // Redundant? Better way to do this?
+        } else if (customerAge < 65 && customerAge >= 18) { // Redundant? Better way to do this?
             sandwichPrice = sandwichPrice;
-        }
-        else System.err.println("Invalid input.");
+        } else System.err.println("Invalid input.");
 
         System.out.printf("Thank you! The price of your sandwich is: $%.2f", sandwichPrice);
     }
